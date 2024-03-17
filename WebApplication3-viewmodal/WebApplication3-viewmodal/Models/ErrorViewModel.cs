cec591f9a9695f9d55ca9f65a328e76ef52b9d6a
Alan <nguyentrungkhanh804@gmail.com>
2024-03-17 20:12:08 +0700

    fsa

---
+++ WebApplication3-viewmodal/WebApplication3-viewmodal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication3_viewmodal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
